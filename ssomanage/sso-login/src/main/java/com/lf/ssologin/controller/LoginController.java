30101ab35a0df8a535d30705e77cb3a79ab5de0a
liufan <1014229769@qq.com>
2022-01-19 22:59:42 +0800

    完成 vip、cart子系统sso跳转

--- ssomanage/sso-login/src/main/java/com/lf/ssologin/controller/LoginController.java
+++ ssomanage/sso-login/src/main/java/com/lf/ssologin/controller/LoginController.java
@@ -2,8 +2,12 @@ package com.lf.ssologin.controller;
 
 import com.lf.ssologin.entity.User;
 import com.lf.ssologin.util.LoginCacheUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -13,26 +17,52 @@ import java.util.UUID;
 @Controller
 public class LoginController {
 
+    /**
+     * 登录
+     *
+     * @param user
+     * @param httpSession
+     * @param response
+     * @return
+     */
     @RequestMapping("/login")
     public String doLogin(User user, HttpSession httpSession, HttpServletResponse response) {
-       String sourceUrl = (String)httpSession.getAttribute("sourceUrl");
+        String sourceUrl = (String) httpSession.getAttribute("sourceUrl");
 
-       if("liufan".equals(user.username) && "111111".equals(user.password))
-       {
-          //登录成功，保存用户信息
-           String token = UUID.randomUUID().toString();
-           LoginCacheUtil.loginUser.put(token,user); LoginCacheUtil.loginUser.put(token,user);
-           //写入cookie
-           Cookie cookie = new Cookie("token",token);
-           cookie.setDomain("codeshop.com");
-           response.addCookie(cookie);
+        if ("liufan".equals(user.username) && "111111".equals(user.password)) {
+            //登录成功，保存用户信息
+            String token = UUID.randomUUID().toString();
+            LoginCacheUtil.loginUser.put(token, user);
+            LoginCacheUtil.loginUser.put(token, user);
+            //写入cookie
+            Cookie cookie = new Cookie("token", token);
+            cookie.setDomain("codeshop.com");
+            response.addCookie(cookie);
 
-       }else{
-           //登录失败,跳转到登录页面
-           httpSession.setAttribute("msg","登录失败!");
-           return "login";
-       }
-           //重定向到source url地址
-        return "redirect:" +sourceUrl;
+        } else {
+            //登录失败,跳转到登录页面
+            httpSession.setAttribute("msg", "登录失败!");
+            return "login";
+        }
+        //重定向到source url地址
+        return "redirect:" + sourceUrl;
+    }
+
+    /**
+     * 通过token获取登录用户信息
+     *
+     * @param token
+     * @return
+     */
+    @RequestMapping("userInfo")
+    @ResponseBody
+    public ResponseEntity<User> getUserInfo(String token) {
+        if (!StringUtils.isEmpty(token)) {
+            User user = LoginCacheUtil.loginUser.get(token);
+            return ResponseEntity.ok(user);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
--- ssomanage/sso-login/src/main/java/com/lf/ssologin/controller/ViewController.java
+++ ssomanage/sso-login/src/main/java/com/lf/ssologin/controller/ViewController.java
@@ -1,10 +1,15 @@
 package com.lf.ssologin.controller;
 
+import com.lf.ssologin.entity.User;
+import com.lf.ssologin.util.LoginCacheUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -12,9 +17,33 @@ import javax.servlet.http.HttpSession;
 public class ViewController {
 
     @RequestMapping("/login")
-    public String toLogin(@RequestParam String sourceUrl, HttpSession httpSession) {
-        // sourceUrl合法性校验
+    public String toLogin(@RequestParam(required = false,defaultValue = "") String sourceUrl,
+                          HttpSession httpSession,
+                         @CookieValue(required = false,value = "token") Cookie cookie) {
+//        if(StringUtils.isEmpty(cookie))
+//        {
+//            //定为到特定页面，页面
+//            sourceUrl = "http://www.codeshop.com:9010";
+//        }
+
+        // 应该还有sourceUrl合法性校验
+
+        //写入session
         httpSession.setAttribute("sourceUrl",sourceUrl);
+
+        //已登录的用户再次访问登录系统时，直接重定向
+        if(cookie!=null)
+        {
+            String token =cookie.getValue();
+            User user = LoginCacheUtil.loginUser.get(token);
+            if(user !=null)
+            {
+                //这里还应该把user信息写如Cooke中，再进行从定向
+                return "redirect:"+sourceUrl;
+            }
+        }
+
+
         return "login";
     }
 }
