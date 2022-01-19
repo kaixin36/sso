32855a3e081b536f3b4d33367e49c702dffbc6ac
liufan <1014229769@qq.com>
2022-01-19 17:28:13 +0800

    vip、cart配置为子项目

---
+++ ssomanage/sso-vip/src/main/java/com/lf/ssovip/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.lf.ssovip.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/view")
+public class ViewController {
+
+    @RequestMapping("/index")
+    public String toIndex() {
+        return "index";
+    }
+}
