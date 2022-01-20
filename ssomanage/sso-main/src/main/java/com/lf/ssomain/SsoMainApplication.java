3fd2b72f343756289cd5fcb18c12056110ec0818
liufan <1014229769@qq.com>
2022-01-20 21:18:36 +0800

    完成main、vip、cart页面登录，还差退出

--- ssomanage/sso-main/src/main/java/com/lf/ssomain/SsoMainApplication.java
+++ ssomanage/sso-main/src/main/java/com/lf/ssomain/SsoMainApplication.java
@@ -2,6 +2,8 @@ package com.lf.ssomain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SsoMainApplication {
@@ -10,4 +12,8 @@ public class SsoMainApplication {
         SpringApplication.run(SsoMainApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- ssomanage/sso-main/src/main/java/com/lf/ssomain/controller/ViewController.java
+++ ssomanage/sso-main/src/main/java/com/lf/ssomain/controller/ViewController.java
@@ -1,14 +1,33 @@
 package com.lf.ssomain.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/view")
 public class ViewController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final String LOGIN_INFO_ADDRESS = "http://login.codeshop.com:9000/userInfo?token=";
 
     @RequestMapping("/index")
-    public String toIndex() {
+    public String toIndex(@CookieValue(required = false, value = "token") Cookie cookie, HttpSession httpSession) {
+        if (cookie != null) {
+            String token = cookie.getValue();
+            if (!StringUtils.isEmpty(token)) {
+                Map resultMap = restTemplate.getForObject(LOGIN_INFO_ADDRESS + token, Map.class);
+                httpSession.setAttribute("loginUser",resultMap);
+            }
+        }
         return "index";
     }
 }
